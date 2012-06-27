b46ebc8d7ddf6333fd9536c3b262533ae309c36a
Thomas Rumbaut <thomas@rumbaut.be>
2012-06-27 20:08:20 +0200

    Change output to play by the rules

--- joomla.sh
+++ joomla.sh
@@ -1,21 +1,17 @@
 #!/bin/sh
 
-echo "\n********** Version 1 **********"
 for file in `locate libraries/joomla/version.php`
 do
-    version=`cat $file | grep '$RELEASE' | sed -e "s/.*= '//g" -e "s/';//g"`
-    versionSub=`cat $file | grep '$DEV_LEVEL' | sed -e "s/.*= '//g" -e "s/';//g"`
-    result=$version"."$versionSub
-    echo "Version: "$result @ $domain
+    masterversion=`cat $file | grep '$RELEASE' | sed -e "s/.*= '//g" -e "s/';//g"`
+    subversion=`cat $file | grep '$DEV_LEVEL' | sed -e "s/.*= '//g" -e "s/';//g"`
+    version=$masterversion"."$subversion
+    domain=`echo $file | awk -F "libraries/joomla/version.php" '{print $1}'`
+    echo "$version $domain"
 done
 
-echo "\n********** Version 2 **********"
 for domain in `locate joomla.xml | grep -v "/plugins/" | grep -v "manifest"`
 do
-    cat $domain  | grep -E "<version>" | sed -e "s:.*<version>::g" -e "s:</version>::g"
-    echo "Version: "$version @ domain
-    #cat $domain  | grep -E "<name>" | sed -e "s:.*<name>::g" -e "s:</name>::g"
+    version=`cat $domain | grep -E "<version>" | sed -e "s:.*<version>::g" -e "s:</version>::g"`
+    domain=`echo $domain | awk -F "joomla/joomla.xml" '{print $1}'`
+    echo "$version $domain"
 done
-
-#Major: Welke domeinnaam en versie
-#Minor:
