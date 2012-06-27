030046524495a81fed525dbae429ae5f215d9084
turanct <spinnewebber_toon@hotmail.com>
2012-06-27 19:35:00 +0200

    Changed output to play by the rules :)

--- wordpress.sh
+++ wordpress.sh
@@ -9,11 +9,11 @@ do
 	# Extract version from wp-includes/version.php
 	output[1]=`cat $file | grep 'wp_version = ' | sed -e "s/.*= '//g" | sed -e "s/';//g"`
 
-	# extract domain
-	output[2]=`echo $file | sed -e "s/\/var\/www\/vhosts\///g" | sed -e "s/\/.*//g"`
-
 	# extract dir
-	output[3]=`echo $file | sed -e "s/wp-includes\/version.php//g"`
+	output[2]=`echo $file | sed -e "s/wp-includes\/version.php//g"`
+
+        # extract domain
+        #output[3]=`echo $file | sed -e "s/\/var\/www\/vhosts\///g" | sed -e "s/\/.*//g"`
 
 	#output
 	echo ${output[@]}
