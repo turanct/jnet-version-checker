525ae564a813e00dae38d814c28d8ffee5110094
turanct <spinnewebber_toon@hotmail.com>
2012-06-27 20:33:42 +0200

    Fixed phpBB output to play by the rules

--- phpBB.sh
+++ phpBB.sh
@@ -1,6 +1,25 @@
 #!/bin/sh
 
-# Zoeken naar phpBB instalaties:
+# Locate wp-includes/version.php
+for file in `locate viewforum.php`
+do
 
-mysql -ukevin -Djnet1771_phpBB -poget8A7Bb --skip-column-names -e"select config_value from phpbb_config where config_name='version';" | grep '[\d\.]*'
 
+	# Get phpBB config file path
+	config=`echo $file | sed -e "s/viewforum\.php/config.php/g"`
+
+	# Get connection details
+	host=`cat $config | grep "\$dbhost" | awk -F " = '" '{print $2}' | sed -e "s/';//g"`
+	dbname=`cat $config | grep "\$dbname" | awk -F " = '" '{print $2}' | sed -e "s/';//g"`
+	dbuser=`cat $config | grep "\$dbuser" | awk -F " = '" '{print $2}' | sed -e "s/';//g"`
+	dbpass=`cat $config | grep "\$dbpasswd" | awk -F " = '" '{print $2}' | sed -e "s/';//g"`
+	dbprefix=`cat $config | grep "\$table_prefix" | awk -F " = '" '{print $2}' | sed -e "s/';//g"`
+
+	# Get version number
+	InstalledVersion=`mysql -u $dbuser -D $dbname -p$dbpass -h $host --skip-column-names -e "select config_value from $dbprefix config where config_name='version';" | grep '[\d\.]*'`
+
+	# Output
+	echo "$InstalledVersion $config"
+
+
+done
